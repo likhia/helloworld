26b807b43c0c7a27e48bceec2800f64f495b0ad1
Lim Li Khia <likhia@hotmail.com>
2021-05-19 23:49:09 +0800

    first commit

---
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -0,0 +1,53 @@
+package org.jboss.as.quickstarts.helloworld;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+@SuppressWarnings("serial")
+@WebServlet("/HelloWorld")
+public class HelloWorldServlet extends HttpServlet {
+
+    static String PAGE_HEADER = "<html><head><title>Test Session Replication</title></head><body>";
+
+    static String PAGE_FOOTER = "</body></html>";
+
+    @Inject
+    HelloService helloService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        HttpSession session = req.getSession();
+        String name = (String) req.getParameter("name");
+
+        if (name != null) {
+                SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyhhmmss");
+                name = name + sdf.format(new Date());
+
+                if(session.getAttribute("replica") == null) {
+                        System.out.println("======== Set session ======== " + name);
+                        session.setAttribute("replica", name);
+                }
+
+        }
+
+        PrintWriter writer = resp.getWriter();
+        writer.println(PAGE_HEADER);
+        writer.println("<h1>" + helloService.createHelloMessage((String) session.getAttribute("replica")) + "</h1>");
+        writer.println(PAGE_FOOTER);
+        writer.close();
+    }
+
+}
